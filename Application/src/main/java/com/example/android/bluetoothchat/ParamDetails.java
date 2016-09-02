cbcd4210533defdc9869f2e1cd9aa6a6e165f892
kenichiabe4ta <sute54webedu@yahoo.co.jp>
2016-09-02 22:41:12 +0900

    ListView用ファイル追加

---
+++ Application/src/main/java/com/example/android/bluetoothchat/ParamDetails.java
@@ -0,0 +1,50 @@
+package com.example.android.bluetoothchat;
+public class ParamDetails {
+    //init_parameter.txt データ配置：no,id,name,value,unit
+    private String Param_no;
+    private String Param_id;
+    private String Param_name;
+    private int Param_value;
+    private String Param_unit;
+
+    public String getParam_no() {
+        return Param_no;
+    }
+
+    public void setParam_no(String param_no) {
+        Param_no = param_no;
+    }
+
+    public String getParam_id() {
+        return Param_id;
+    }
+
+    public void setParam_id(String param_id) {
+        Param_id = param_id;
+    }
+
+    public String getParam_name() {
+        return Param_name;
+    }
+
+    public void setParam_name(String param_name) {
+        Param_name = param_name;
+    }
+
+    public int getParam_value() {
+        return Param_value;
+    }
+
+    public void setParam_value(int param_value) {
+        Param_value = param_value;
+    }
+
+    public String getParam_unit() {
+        return Param_unit;
+    }
+
+    public void setParam_unit(String param_unit) {
+        Param_unit = param_unit;
+    }
+
+}
