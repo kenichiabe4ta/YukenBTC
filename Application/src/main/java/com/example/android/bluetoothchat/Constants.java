ddde17526b90e5f3369c0d5ce88f6515514236a8
kenichiabe4ta <sute54webedu@yahoo.co.jp>
2016-09-14 00:28:39 +0900

    コメント追加

--- Application/src/main/java/com/example/android/bluetoothchat/Constants.java
+++ Application/src/main/java/com/example/android/bluetoothchat/Constants.java
@@ -32,7 +32,7 @@ public interface Constants {
     public static final String DEVICE_NAME = "device_name";
     public static final String TOAST = "toast";
 
-    //
+    // 
     // 通信コマンド(パラメータno,value指定不要のもの)
     public static final byte[] COMM_s = "s\r\n".getBytes();// LED点灯
     public static final byte[] COMM_r = "r\r\n".getBytes();// LED消灯
